c37aa4df08b71bbc1d52c2d67212d650aa44761b
Sodium Bismuth <nathan.bizon@viacesi.fr>
2026-02-04 16:48:52 +0100

    Feat : new test for ApplicationConfiguration

---
+++ EasySaveTest/AppConfigInitTest.cs
@@ -0,0 +1,13 @@
+﻿namespace EasySaveTest;
+
+public class AppConfigInitTest
+{
+    [Test]
+    public void AppConfigThrowExceptionIfInstanceCalledBeforeLoading()
+    {
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            var instance = EasySave.ApplicationConfiguration.Instance;
+        });
+    }
+}
