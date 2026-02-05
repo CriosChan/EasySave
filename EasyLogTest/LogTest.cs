dba1d8b692071252932eb5da2081b5b6fccd4469
Yasser TOUMI <149902910+yass170@users.noreply.github.com>
2026-02-05 09:50:46 +0100

    v1.0

--- EasyLogTest/LogTest.cs
+++ EasyLogTest/LogTest.cs
@@ -23,23 +23,23 @@ public class Tests
             FileTarget = "./Test1.txt",
             Time = DateTime.Now
         };
-        
+
         string logPath = Path.Join("./", DateTime.Now.ToString("yyyy-MM-dd") + ".json");
         // If the file already exists remove it.
-        if(File.Exists(logPath))
+        if (File.Exists(logPath))
             File.Delete(logPath);
         // Log the objet
         _logger.Log(log);
         // Check if the log file exists
         Assert.That(File.Exists(logPath), Is.EqualTo(true));
         // Get the file content
-        string json = File.ReadAllText(logPath);
-        List<FakeLogObject> logs = JsonSerializer.Deserialize<List<FakeLogObject>>(json) ?? new();
-        Assert.That(logs.Count, Is.GreaterThan(0));
-
+        IEnumerable<string> lines = File.ReadLines(logPath);
+        Assert.That(lines.Count(), Is.Not.EqualTo(0));
+        // Deserialize it
+        FakeLogObject readedLog = JsonSerializer.Deserialize<FakeLogObject>(lines.ElementAt(0));
         // Check if the first one is equal to the object we gave
-        Assert.That(logs[0], Is.EqualTo(log));
-        
+        Assert.That(readedLog, Is.EqualTo(log));
+
         // Change some values 
         log.Name = "Test2";
         log.FileSource = "./Test2.txt";
@@ -48,11 +48,9 @@ public class Tests
         // Log a second object
         _logger.Log(log);
         // Read again
-        json = File.ReadAllText(logPath);
-        logs = JsonSerializer.Deserialize<List<FakeLogObject>>(json) ?? new();
-        Assert.That(logs.Count, Is.GreaterThanOrEqualTo(2));
-
+        lines = File.ReadLines(logPath);
         // And check again if the second log is correct
-        Assert.That(logs[1], Is.EqualTo(log));
+        readedLog = JsonSerializer.Deserialize<FakeLogObject>(lines.ElementAt(1));
+        Assert.That(readedLog, Is.EqualTo(log));
     }
 }
