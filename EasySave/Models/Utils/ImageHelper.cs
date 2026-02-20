981e9351676d84f782cc62b770792f813cc93c63
CriosChan <thomas.notolruob@gmail.com>
2026-02-20 15:45:30 +0100

    docs(playpausestop)

--- EasySave/Models/Utils/ImageHelper.cs
+++ EasySave/Models/Utils/ImageHelper.cs
@@ -3,17 +3,28 @@ using Avalonia.Platform;
 
 namespace EasySave.Models.Utils;
 
+/// <summary>
+/// Provides utilities for image operations, specifically loading images from resources.
+/// </summary>
 public class ImageHelper
 {
-        public static Bitmap? LoadFromResource(Uri resourceUri)
+    /// <summary>
+    /// Loads a bitmap image from a specified resource URI.
+    /// </summary>
+    /// <param name="resourceUri">The URI of the resource to load the image from.</param>
+    /// <returns>A Bitmap object if successful; otherwise, null.</returns>
+    public static Bitmap? LoadFromResource(Uri resourceUri)
+    {
+        try
         {
-            try
-            {
-                return new Bitmap(AssetLoader.Open(resourceUri));
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
+            // Attempt to load the bitmap image from the provided resource URI.
+            return new Bitmap(AssetLoader.Open(resourceUri));
         }
+        catch (Exception e)
+        {
+            // Log or handle the exception as needed. 
+            // Currently, it simply returns null if any exception occurs.
+            return null;
+        }
+    }
 }
